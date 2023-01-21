6052ae7948fed30edda3bb10e62dd44b96318545
theshenron <gourav143faz@gmail.com>
2023-01-21 12:16:40 +0530

    server done

---
+++ controllers/userController.js
@@ -0,0 +1,71 @@
+const asyncHandler = require('express-async-handler')
+const generateToken = require('../config/generateToken')
+const userModel = require('../model/user.model')
+
+
+
+const registerUser = asyncHandler( async (req,res)=>{
+
+    console.log(req.body)
+    
+    const {name , email , password } = req.body
+
+    if(!name  || !email || !password){
+        res.status(201)
+        throw new Error("Fill All The Fields")
+    }
+
+
+    // const userExist = await 
+    const userExists = await userModel.findOne({ email })
+    if (userExists) {
+        throw new Error('User Already Exist')
+    }
+
+
+    const user = await userModel.create({
+        name,
+        email,
+        password,
+    })
+
+
+    if (user) {
+        res.status(201).send({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Fail to Create User')
+    }
+
+})
+
+
+
+const authUser = asyncHandler(async (req, res) => {
+
+    const { email, password } = req.body
+
+    //finding user in db
+    const user = await userModel.findOne({ email })
+
+    if (user && (await user.matchPassword(password))) {
+        res.status(201).send({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('User not found or Password is Incorrect')
+    }
+})
+
+
+
+module.exports = { registerUser, authUser }
